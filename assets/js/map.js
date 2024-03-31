37486620407a2d4e988ca5c7b2348bcb48bba35d
Victornrib <victornrib@gmail.com>
2024-03-31 08:46:59 +0200

    Loading focuscrimes maps

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,44 +1,15 @@
-var crimeTypes = ['WEAPON LAWS', 'PROSTITUTION', 'DRIVING UNDER THE INFLUENCE', 'ROBBERY', 'BURGLARY', 'ASSAULT', 'DRUNKENNESS', 'DRUG/NARCOTIC', 'TRESPASS', 'LARCENY/THEFT', 'VANDALISM', 'VEHICLE THEFT', 'STOLEN PROPERTY', 'DISORDERLY CONDUCT'];
+function loadSelectedMap() {
+    var mapSelection = document.getElementById("map-selection");
+    var selectedMap = mapSelection.value;
 
-var dropdown = document.getElementById('crimeType');
-for (var i = 0; i < crimeTypes.length; i++) {
-    var option = document.createElement('option');
-    option.text = crimeTypes[i];
-    option.value = crimeTypes[i];
-    dropdown.appendChild(option);
+    var mapContainer = document.getElementById("map-container");
+    mapContainer.innerHTML = "";
+
+    var iframe = document.createElement("iframe");
+    iframe.src = "assets/plots/maps/" + selectedMap + ".html";
+    iframe.style.width = "100%";
+    iframe.style.height = "500px";
+    mapContainer.appendChild(iframe);
 }
 
-document.getElementById('crimeType').addEventListener('change', function() {
-    var selectedCrimeType = this.value;
-    // var newData = getCrimeData(selectedCrimeType);
-    // updateMapPlot(newData);
-});
-
-// function updateMapPlot(newData) {
-//     var plot = document.getElementById('map-visualization');
-//     Plotly.react(plot, newData, layout);
-// }
-
-// function getCrimeData(crimeType) {
-//     if (crimeType === 'WEAPON LAWS') {
-//         return weaponLawsData;
-//     } else if (crimeType === 'PROSTITUTION') {
-//         return prostitutionData;
-//     }
-// }
-
-// var layout = {
-//     title: 'San Francisco Crime Map',
-//     mapbox: {
-//         style: 'carto-positron',
-//         center: { lat: 37.7749, lon: -122.4194 },
-//         zoom: 10
-//     }
-// };
-// var weaponLawsData = {
-// };
-
-// var prostitutionData = {
-// };
-
-// updateMapPlot(getCrimeData('WEAPON LAWS'));
+document.getElementById("map-selection").addEventListener("change", loadSelectedMap);
