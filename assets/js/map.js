f8c8a28382d920a034a4508b5ded0d4c3ef56c15
Victornrib <victornrib@gmail.com>
2024-03-31 09:43:52 +0200

    bug fixing

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function loadSelectedMap() {
-        var mapSelection = document.getElementById("map-selection");
-        var selectedMap = mapSelection.value;
-
-        var mapContainer = document.getElementById("map-container");
-        mapContainer.innerHTML = "";
-
-        var iframe = document.createElement("iframe");
-        var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
-        iframe.src = pathString;
-        iframe.style.width = "100%";
-        iframe.style.height = "500px";
-        mapContainer.appendChild(iframe);
-    }
-
-    document.getElementById("map-selection").addEventListener("change", loadSelectedMap);
-
     loadSelectedMap();
 });
+
+function loadSelectedMap() {
+    var mapSelection = document.getElementById("map-selection");
+    var selectedMap = mapSelection.value;
+
+    var mapContainer = document.getElementById("map-container");
+    mapContainer.innerHTML = "";
+
+    var iframe = document.createElement("iframe");
+    var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
+    iframe.src = pathString;
+    iframe.style.width = "100%";
+    iframe.style.height = "500px";
+    mapContainer.appendChild(iframe);
+}
