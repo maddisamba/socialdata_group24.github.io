ac328d238c1ff7b6e07a5f4c502f9237a2d8ec1c
Victornrib <victornrib@gmail.com>
2024-03-31 09:35:54 +0200

    loading assault crime by default

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,12 +7,12 @@ function loadSelectedMap() {
 
     var iframe = document.createElement("iframe");
     var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
-    console.log(pathString)
     iframe.src = pathString;
-    console.log(iframe.src)
     iframe.style.width = "100%";
     iframe.style.height = "500px";
     mapContainer.appendChild(iframe);
 }
 
 document.getElementById("map-selection").addEventListener("change", loadSelectedMap);
+
+loadSelectedMap();
