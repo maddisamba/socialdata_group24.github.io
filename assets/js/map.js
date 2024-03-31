e877a744dcea74050c8b5928b47f6dae8b35a5bc
Victornrib <victornrib@gmail.com>
2024-03-31 09:40:49 +0200

    waiting for document to be ready

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,18 +1,20 @@
-function loadSelectedMap() {
-    var mapSelection = document.getElementById("map-selection");
-    var selectedMap = mapSelection.value;
+document.addEventListener("DOMContentLoaded", function() {
+    function loadSelectedMap() {
+        var mapSelection = document.getElementById("map-selection");
+        var selectedMap = mapSelection.value;
 
-    var mapContainer = document.getElementById("map-container");
-    mapContainer.innerHTML = "";
+        var mapContainer = document.getElementById("map-container");
+        mapContainer.innerHTML = "";
 
-    var iframe = document.createElement("iframe");
-    var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
-    iframe.src = pathString;
-    iframe.style.width = "100%";
-    iframe.style.height = "500px";
-    mapContainer.appendChild(iframe);
-}
+        var iframe = document.createElement("iframe");
+        var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
+        iframe.src = pathString;
+        iframe.style.width = "100%";
+        iframe.style.height = "500px";
+        mapContainer.appendChild(iframe);
+    }
 
-document.getElementById("map-selection").addEventListener("change", loadSelectedMap);
+    document.getElementById("map-selection").addEventListener("change", loadSelectedMap);
 
-loadSelectedMap();
+    loadSelectedMap();
+});
