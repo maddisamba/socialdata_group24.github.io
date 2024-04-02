e33c850958156d23f521a923e230ffaf79332e6e
Victornrib <victornrib@gmail.com>
2024-04-02 18:33:31 +0200

    testing new map format

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,6 +11,7 @@ function loadSelectedMap() {
     mapContainer.innerHTML = "";
 
     var iframe = document.createElement("iframe");
+    iframe.setAttribute("frameborder","0")
     var pathString = "https://maddisamba.github.io/socialdata_group24.github.io/assets/plots/maps/" + selectedMap + ".html";
     iframe.src = pathString;
     iframe.style.width = "100%";
